1533944fd44ca4d69346f3251b1c448684585386
glowysourworm <ryanddolan@hotmail.com>
2025-03-27 16:23:13 -0500

    basic tab controls functioning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,60 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgClass, NgIf, NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'sociallinks',
+  templateUrl: './component/sociallinks.html'
+})
+export class SocialLinksComponent {
+}
+
+// Tab Components
+
+export class Tab {
+  name: string;
+  index: number;
+  constructor(theName: string, theIndex: number) {
+    this.name = theName;
+    this.index = theIndex;
+  }
+}
+
+@Component({
+  selector: 'maintab',
+  templateUrl: './component/maintab.html'
+})
+export class MainTabComponent {
+}
+
+@Component({
+  selector: 'chattab',
+  templateUrl: './component/chattab.html'
+})
+export class ChatTabComponent {
+
+}
 
 @Component({
   selector: 'chatbox',
-  templateUrl: './app.chatbox.html',
-  styleUrl: './app.chatbox.css'
+  templateUrl: './component/chatbox.html'
 })
 export class ChatBoxComponent {
 }
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChatBoxComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet,  NgOptimizedImage, NgIf, NgClass, SocialLinksComponent, MainTabComponent, ChatTabComponent, ChatBoxComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'sm-esports-web';
+
+  tabs = [new Tab('Business Center', 0), new Tab('Chat', 1)];
+
+  selectedTab: Tab;
+
+  constructor() {
+    this.selectedTab = this.tabs[0];
+  }
 }
